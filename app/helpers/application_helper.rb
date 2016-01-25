7f478010bbdb46f23f70492815153c2417a41566
pebiantara <pebiantara@gmail.com>
2016-01-25 19:40:44 +0700

    fixed calculation jackpot date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,40 @@
 module ApplicationHelper
 
   def powerball_jackpot
-    # Saturday night at 10:59 p.m. Eastern Time
-    powerball_date = array_days_of_the_week[3]
-    Time.parse(powerball_date.to_s + " 10:59 pm").in_time_zone.strftime("%d %b %Y %H:%M:00 EST")    
+    #Every Wednesday and Saturday night at 10:59 p.m
+    calculate_date(3, 6, "10:59")
   end
 
   def mega_jackpot
     # Tuesday and Friday at 11:00 p.m
-    mega_date = array_days_of_the_week[4]
-    Time.parse(mega_date.to_s + " 11:00 pm").in_time_zone.strftime("%d %b %Y %H:%M:00 EST")
+    calculate_date(2, 5, "10:59")
   end
 
   def lotto_jackpot
-    # Saturday night at 11:15 p.m.
-    lotto_date = array_days_of_the_week[5]
-    Time.parse(lotto_date.to_s + " 11:15 pm").in_time_zone.strftime("%d %b %Y %H:%M:00 EST")
+    # Every Wednesday and Saturday night
+    calculate_date(3, 6, "10:59")
   end
 
-  def array_days_of_the_week
+  def calculate_date(index_day_1, index_day_2, jackpot_time)
+    next_jackpot = array_days_of_the_week[index_day_1]
+    jackpot_date = Time.parse(next_jackpot.to_s + " #{jackpot_time}").in_time_zone.strftime("%d %b %Y %H:%M:00 EST")
+    now = Time.now
+    if jackpot_date < now.in_time_zone.strftime("%d %b %Y %H:%M:00 EST")
+      next_jackpot = array_days_of_the_week[index_day_2]
+      jackpot_date = Time.parse(next_jackpot.to_s + " #{jackpot_time}").in_time_zone.strftime("%d %b %Y %H:%M:00 EST")
+      if jackpot_date < now.in_time_zone.strftime("%d %b %Y %H:%M:00 EST")
+	      next_jackpot = array_days_of_the_week(true)[index_day_1]
+	      jackpot_date = Time.parse(next_jackpot.to_s + " #{jackpot_time}").in_time_zone.strftime("%d %b %Y %H:%M:00 EST")
+        return jackpot_date
+      end
+      return jackpot_date
+    end
+    jackpot_date
+  end
+
+  def array_days_of_the_week(next_week=false)
     Time.zone = "Eastern Time (US & Canada)"
-    a = Date.today
+    a = next_week ? Date.today + 1.week : Date.today
     (a.at_beginning_of_week..a.at_end_of_week).map.each{|x| x}
   end
 end
