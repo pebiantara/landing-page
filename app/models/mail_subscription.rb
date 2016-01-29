e1093ecb372580fbe961527b1572a86a02320590
pebiantara <pebiantara@gmail.com>
2016-01-30 01:39:32 +0700

    change text and flash message

--- app/models/mail_subscription.rb
+++ app/models/mail_subscription.rb
@@ -1,5 +1,5 @@
 class MailSubscription < ActiveRecord::Base
   validates_presence_of :email, message: "Email can't be blank"
   validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: 'Invalid email address.'
-  validates_uniqueness_of :email, message: "Oops! We are currently waitlisting new subscribers, and will notify you when openings are available. Thank you :)"
+  validates_uniqueness_of :email, message: "You are already in our waiting list. Will notify you when openings are available. Thank you : )"
 end
